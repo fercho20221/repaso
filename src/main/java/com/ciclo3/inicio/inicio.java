7d552c4c699d501147ee39de7e5388a74e202e1d
Fernando <fercu1@hotmail.com>
2022-08-13 09:23:34 -0500

    quinto

--- src/main/java/com/ciclo3/inicio/inicio.java
+++ src/main/java/com/ciclo3/inicio/inicio.java
@@ -4,5 +4,7 @@ public class inicio {
     public static void main(String[] args) {
         System.out.println("Hola developers");
         System.out.println("Hola otro");
+        System.out.println("Hola otro2");
+        System.out.println("Hola otro3");
     }
 }
